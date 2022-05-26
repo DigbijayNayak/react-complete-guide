213da7650de99aab1fc6d7b5c3009df190872829
Digbijay Nayak <digbijaynayak522@gmail.com>
2022-05-26 18:58:25 +0530

    practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: "Bike Insurance", amount: 294.67, date: new Date(2021, 3, 28) },
+    { title: "Tractor Insurance", amount: 294.67, date: new Date(2021, 4, 28) },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date = {expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date = {expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date = {expenses[2].date}></ExpenseItem>
+
+      
     </div>
   );
 }
