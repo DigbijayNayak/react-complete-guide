ce7d87b2087b7d2beec2f375413c9a2eae55250e
Digbijay Nayak <digbijaynayak522@gmail.com>
2022-05-30 17:26:12 +0530

    Workign with components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from './components/Expenses/Expenses'
 function App() {
   const expenses = [
     { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
@@ -10,11 +10,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date = {expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date = {expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date = {expenses[2].date}></ExpenseItem>
-
-      
+      <Expenses items={expenses}/>
     </div>
   );
 }
